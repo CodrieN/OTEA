1a261ef7cf2cbfad2b75230e94d0fc268bf6782e
CodrieN <adrien.marguin@gmail.com>
2023-03-22 22:02:52 +0100

    ajout de l'effet blur, travail sur les transitions keyframes card 1

--- function.js
+++ function.js
@@ -1,3 +1,19 @@
+/* -- Glow effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
+};
+
 /* -- Text effect -- */
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -5,30 +21,29 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
 
 const screen = document.querySelector(".screen"),
-      name = document.querySelector(".name");
+  name = document.querySelector(".name");
 
-screen.onmouseenter = event => {  
+screen.onmouseenter = (event) => {
   let iteration = 0;
-  
+
   clearInterval(interval);
-  
+
   interval = setInterval(() => {
     name.innerText = name.innerText
       .split("")
       .map((letter, index) => {
-        if(index < iteration) {
+        if (index < iteration) {
           return name.dataset.value[index];
         }
-      
-        return letters[Math.floor(Math.random() * 26)]
+
+        return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
-    
-    if(iteration >= name.dataset.value.length){ 
+
+    if (iteration >= name.dataset.value.length) {
       clearInterval(interval);
     }
-    
+
     iteration += 1 / 3;
   }, 30);
-}
-
+};
